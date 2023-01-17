cf40107b03258db40d53fe7ac5d81339eec30dc5
4ng3lic4 <ositosviajeros@hotmail.com>
2023-01-17 10:07:14 -0500

    add comments model

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,17 +1,11 @@
-//Import  packages
+//Import  modules
 const router = require('express').Router();
  const userRoutes = require('./userRoutes');
  const postRoutes = require('./post-routes');
-
-
-//the variable doesn't allow me to use a dash in the name, so I did CamelCase instead
-const commentRoutes = require('/comments-routes');
-const postRoutes= require('/post-routes');
-const UserRoutes = require('/User-routes');
-
+ const commentRoutes = require('./comments-routes')
   
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
-router.use('/connebts', commentRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;
