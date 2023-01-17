5ba8f535a982190a53e228d8868f7bf07d0b4607
4ng3lic4 <ositosviajeros@hotmail.com>
2023-01-17 09:47:15 -0500

    updating api routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,14 +1,17 @@
-//connect all of the routes from this api folder
+
 const router = require('express').Router();
+//connect all of the routes from this api folder
+const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes');
+const dashboardRoutes = require('./dashboard-routes');
 
-const userRoutes = require('./userRoutes');
-const postRoutes = require('./post-routes');
-const commentRoutes = require('./comments-routes');
 
+router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
+router.use('/dashboard', dashboardRoutes);
 
-
-router.use('/users', userRoutes);
-router.use('/posts', postRoutes);
-router.use('/comments', commentRoutes);
+router.use((req,res)=> {
+    res.status(404).end();
+});
 
 module.exports = router;
